d3d8963f064928e0cff9c34ad4e93522d8d9246f
hta55 <hta55@uclive.ac.nz>
2018-02-25 14:07:08 +1300

    this is a test commit to check if git is working on local machine

--- src/main/java/seng302/App.java
+++ src/main/java/seng302/App.java
@@ -5,5 +5,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welcome to SENG302" );
+        System.out.println( "Test for first commit" );
     }
 }
