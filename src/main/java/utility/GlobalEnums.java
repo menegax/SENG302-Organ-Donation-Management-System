fd11f8343aa2080f51128acc5d3d5943fc381580
hta55 <hta55@uclive.ac.nz>
2018-02-26 09:56:39 +1300

    implementation of the enums class.

--- src/main/java/utility/GlobalEnums.java
+++ src/main/java/utility/GlobalEnums.java
@@ -2,4 +2,55 @@ package utility;
 
 public class GlobalEnums {
 
+    public enum Gender {
+        MALE,
+        FEMALE,
+        OTHER,
+    }
+
+    public enum Reigon {
+        NORTHLAND,
+        AUCKLAND,
+        WAIKATO,
+        BAYOFPLENTY,
+        GISBORNE,
+        HAWKESBAY,
+        TARANAKI,
+        MANAWATU,
+        WELLINGTON,
+        TASMAN,
+        NELSON,
+        MARLBOROUGH,
+        WESTCOAST,
+        CANTERBURY,
+        OTAGO,
+        SOUTHLAND
+    }
+
+    public enum Organ {
+        LIVER,
+        KIDNEY,
+        PANCREAS,
+        HEART,
+        LUNG,
+        INTESTINE,
+        CORNEA,
+        MIDDLEEAR,
+        SKIN,
+        BONE,
+        BONEMARROW,
+        CONNECTIVETISSUE
+    }
+
+    public enum BloodGroup {
+        A_POSTIVE,
+        A_NEGATIVE,
+        B_POSTIVE,
+        B_NEGATIVE,
+        AB_POSITIVE,
+        AB_NEGATIVE,
+        O_POSITVE,
+        O_NEGATIVE
+    }
+
 }
