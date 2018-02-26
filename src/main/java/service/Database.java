33de4f5997376b2e22dee96c84023f5d4300aac1
Andrew Spearman <asp79@uclive.ac.nz>
2018-02-26 21:12:31 +1300

    Added basic database methods for the stored Donor HashSet

--- src/main/java/service/Database.java
+++ src/main/java/service/Database.java
@@ -1,5 +1,30 @@
 package service;
 
+import java.util.HashSet;
+
+import model.Donor;
+
 public class Database {
+
+    private HashSet<Donor> donors;
+
+
+    public HashSet<Donor> getDonors() {
+        return donors;
+    }
+
+    public void addDonor(Donor newDonor) {
+        donors.add(newDonor);
+    }
+
+    public void removeDonor(Donor donor) {
+        donors.remove(donor);
+    }
+
+    public void updateDonor(Donor newDonor) {
+        removeDonor(newDonor);
+        addDonor(newDonor);
+    }
+
 
 }
