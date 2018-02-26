155caee0ed1e61e26cdb5995cd52bc1f47355fc2
Andrew Spearman <asp79@uclive.ac.nz>
2018-02-27 12:04:29 +1300

    Reinstated the database Donor ID method, but commented out

--- src/main/java/service/Database.java
+++ src/main/java/service/Database.java
@@ -25,4 +25,24 @@ public class Database {
         addDonor(newDonor);
     }
 
+//    public static int getMaxDonorId(){
+//        int maxId = 0;
+//        if (getDonors().isEmpty()) return 0; // no donors yet,
+//        for (Donor d : getDonors()){
+//            if (d.getDonorId() > maxId){
+//                maxId = d.getDonorId();
+//            }
+//        }
+//        return maxId;
+//    }
+//
+//    public static Donor getDonorById(int id) throws DonorNotFoundException {
+//        for (Donor d : getDonors()){
+//            if (d.getDonorId() == id){
+//                return d;
+//            }
+//        }
+//        throw new DonorNotFoundException();
+//    }
+
 }
