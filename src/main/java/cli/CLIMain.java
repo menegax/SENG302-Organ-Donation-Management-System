428455c05179833e86461f67b1c7d05307713c4f
Andrew Spearman <asp79@uclive.ac.nz>
2018-03-08 14:32:08 +1300

    Minor cleaning of main() CLI

--- src/main/java/cli/CLIMain.java
+++ src/main/java/cli/CLIMain.java
@@ -6,14 +6,6 @@ public class CLIMain {
 
     public static void main(String[] argv) {
         String[] args;
-//        System.out.println("+  xxxxx   xxxxxxx  xx       xx    xxxxxxxxxx" + "\n" +
-//                           "| x     x  x     xx xxx     xxx    x" + "\n" +
-//                           "| x     x  x      x x xx    x xx   xx" + "\n" +
-//                           "| x     x  x      x x   xx xx  x    xxxxxxxx" + "\n" +
-//                           "| x     x  x     xx x    xxx   xx          xx" + "\n" +
-//                           "|  xxxxx   xxxxxxx  x     xx    xx xxxxxxxxxx" + "\n" +
-//                           "+----------------------------------------------+" + "\n"
-//        );
 
         // listen for input
         Scanner inputScanner = new Scanner(System.in);
