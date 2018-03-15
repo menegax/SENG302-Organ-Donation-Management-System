aad1837cc312a0b53170c30500ddb6f4c8822b27
hta55 <hta55@uclive.ac.nz>
2018-03-15 13:13:44 +1300

    Implementation for commmandLine history
    -created terminal builder and readline objects
    -changed prompt to "odms>>"

--- src/main/java/cli/CLIMain.java
+++ src/main/java/cli/CLIMain.java
@@ -4,8 +4,6 @@ import org.jline.reader.LineReaderBuilder;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 import picocli.CommandLine;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class CLIMain {
@@ -15,18 +13,14 @@ public class CLIMain {
         try {
             TerminalBuilder builder = TerminalBuilder.builder();
             Terminal terminal = builder.build();
-            LineReader reader = LineReaderBuilder.builder()
-                    .terminal(terminal)
-                    .build();
-
+            LineReader reader = LineReaderBuilder.builder().terminal(terminal).build();
             String userCommand;
             new CommandLine(new CLIOdms()).parseWithHandler(new CommandLine.RunLast(), System.err, "-h");
-            userCommand = reader.readLine();
-            System.out.println(userCommand);
+            userCommand = reader.readLine("odms>>");
             while (!userCommand.trim().equals("quit")) {
                 args = userCommand.split(" ");
                 new CommandLine(new CLIOdms()).parseWithHandler(new CommandLine.RunLast(), System.err, args);
-                userCommand = reader.readLine(">>");
+                userCommand = reader.readLine("odms>>");
             }
         } catch (IOException e) {
             e.printStackTrace();
