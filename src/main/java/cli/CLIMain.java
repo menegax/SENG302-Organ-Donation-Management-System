56d8018f97852481fcc06d6526923641a0611831
hta55 <hta55@uclive.ac.nz>
2018-03-15 13:01:13 +1300

    JLine library addition
    -added to pom
    -added in skeleton code for line terminal builder
    -removed listener class
    
    #story[147] #pair[hta55, kla98]

--- src/main/java/cli/CLIMain.java
+++ src/main/java/cli/CLIMain.java
@@ -1,53 +1,35 @@
 package cli;
-import jline.console.ConsoleReader;
-import org.jnativehook.GlobalScreen;
-import org.jnativehook.NativeHookException;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 import picocli.CommandLine;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class CLIMain {
 
     public static void main(String[] argv) {
         String[] args;
-
-        // setup keyboard listener
-        /**try {
-            GlobalScreen.registerNativeHook();
-        }
-        catch (NativeHookException ex) {
-            System.err.println("There was a problem registering the native hook.");
-            System.err.println(ex.getMessage());
-
-            System.exit(1);
-        }
-
-        GlobalScreen.addNativeKeyListener(new CLIKeyboardListener());**/
         try {
-            ConsoleReader consoleReader = new ConsoleReader();
-            consoleReader.addTriggeredAction('a', new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    System.out.println("a press");
-                }
-            });
+            TerminalBuilder builder = TerminalBuilder.builder();
+            Terminal terminal = builder.build();
+            LineReader reader = LineReaderBuilder.builder()
+                    .terminal(terminal)
+                    .build();
+
+            String userCommand;
+            new CommandLine(new CLIOdms()).parseWithHandler(new CommandLine.RunLast(), System.err, "-h");
+            userCommand = reader.readLine();
+            System.out.println(userCommand);
+            while (!userCommand.trim().equals("quit")) {
+                args = userCommand.split(" ");
+                new CommandLine(new CLIOdms()).parseWithHandler(new CommandLine.RunLast(), System.err, args);
+                userCommand = reader.readLine(">>");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        // listen for input
-        Scanner inputScanner = new Scanner(System.in);
-        String userCommand;
-        new CommandLine(new CLIOdms()).parseWithHandler(new CommandLine.RunLast(), System.err, "-h");
-        userCommand = inputScanner.nextLine();
-        while (!userCommand.trim().equals("quit")) {
-            args = userCommand.split(" ");
-            new CommandLine(new CLIOdms()).parseWithHandler(new CommandLine.RunLast(), System.err, args);
-            userCommand = inputScanner.nextLine();
-        }
     }
-
 }
