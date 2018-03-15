0e2e8e71bb66746dbbfb0b20047e52762a5cb60f
hta55 <hta55@uclive.ac.nz>
2018-03-15 15:23:28 +1300

    Adding formatter class for ActionHistory
    -missed file from previous commit

---
+++ src/main/java/utility/FormatterLog.java
@@ -0,0 +1,11 @@
+package utility;
+import org.apache.commons.lang3.StringUtils;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class FormatterLog extends Formatter {
+    @Override
+    public String format(LogRecord record) {
+        return record.getLevel() + ": " + StringUtils.capitalize(record.getMessage()) + "\n";
+    }
+}
