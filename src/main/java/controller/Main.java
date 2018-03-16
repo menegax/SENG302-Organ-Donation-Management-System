44ae3bc5d2d0dd1e0cb8da18cc22fa4f93bd266c
Andrew Spearman <asp79@uclive.ac.nz>
2018-03-16 13:40:00 +1300

    Added logging functionality to start with GUI main
    #story[154]

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import utility.UserActionHistory;
 
 public class Main extends Application {
 
@@ -19,6 +20,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+        UserActionHistory.setup(); // start logs
         launch(args);
     }
 }
