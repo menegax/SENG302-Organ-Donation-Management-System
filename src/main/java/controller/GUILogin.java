7fc380fc5ea2b53dcf93dfd8fa16d5726d5f9ac8
Andrew Spearman <asp79@uclive.ac.nz>
2018-03-16 14:49:43 +1300

    Linking login, home, and history screens
    #story[154]

--- src/main/java/controller/GUILogin.java
+++ src/main/java/controller/GUILogin.java
@@ -8,11 +8,19 @@ import java.io.InvalidObjectException;
 
 public class GUILogin {
 
+    /**
+     * Open the register screen
+     */
     @FXML
     public void goToRegister(){
         GUIScreenControl.activate("donorRegister");
     }
 
+    /**
+     * Attempt to log the user in using the entered NHI
+     * If successful, opens home screen
+     * If failed, gives alert
+     */
     @FXML
     public void logIn(){
         // todo surround with try catch. Try uses database getuserbyNHI, catch will throw a popup with error alert
