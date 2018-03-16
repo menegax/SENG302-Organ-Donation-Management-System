a97123999f68d19d75c9f9b4df0ed235fa64595b
Kyle <kla98@uclive.ac.nz>
2018-03-16 16:09:42 +1300

    Got most of the basic variable loading working on the profile page. #story[154]

--- src/main/java/controller/GUIDonorProfile.java
+++ src/main/java/controller/GUIDonorProfile.java
@@ -1,9 +1,79 @@
 package controller;
 
+import model.Donor;
+import service.Database;
+import utility.GlobalEnums;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.io.InvalidObjectException;
+
 public class GUIDonorProfile {
 
+    @FXML
+    private Label nhiLbl;
+
+    @FXML
+    private Label nameLbl;
+
+    @FXML
+    private Label genderLbl;
+
+    @FXML
+    private Label dobLbl;
+
+    @FXML
+    private Label heightLbl;
+
+    @FXML
+    private Label weightLbl;
+
+    @FXML
+    private Label bloodGroupLbl;
+
+    @FXML
+    private Label addLbl1;
+    @FXML
+    private Label addLbl2;
+    @FXML
+    private Label addLbl3;
+    @FXML
+    private Label addLbl4;
+
+    @FXML
+    private Label donationList;
+
+    public void initialize() {
+
+        loadProfile("ABC1238");
+    }
+
+    public void loadProfile(String nhi) {
+        try {
+            Donor donor = Database.getDonorByNhi(nhi);
+
+            nhiLbl.setText(donor.getNhiNumber());
+            nameLbl.setText(donor.getNameConcatenated());
+            //genderLbl.setText(Genderdonor.getGender()); //TODO figure out enum string
+            dobLbl.setText("TODO");
+            heightLbl.setText(String.valueOf(donor.getHeight()));
+            weightLbl.setText(String.valueOf(donor.getWeight()));
+            //bloodGroupLbl.setText(donor.getBloodGroup().getValue());
+            addLbl1.setText("TODO ADD_1");
+            addLbl2.setText("TODO ADD_2");
+            addLbl3.setText("TODO ADD_3");
+            addLbl4.setText("TODO ADD_4");
+            donationList.setText("ONE\nTWO\nTHREE");
+        } catch(InvalidObjectException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
     public void goToHome() {
         GUIScreenControl.activate("home");
     }
+
+
 }
--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -5,7 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.Donor;
+import service.Database;
 import utility.UserActionHistory;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Main extends Application {
 
@@ -16,6 +21,10 @@ public class Main extends Application {
         primaryStage.setScene(rootScene); //set scene on primary stage
         GUIScreenControl.setRootScene(rootScene); // set this scene in screen controller
 
+        //TODO Remove below donor creation after testing
+        ArrayList<String> dal = new ArrayList<>();
+        dal.add("Middle");
+        Database.addDonor(new Donor("ABC1238", "Joe", dal,"Bloggs", LocalDate.of(1990, 2, 9)));
         //TODO: if you have a FXML file, please add it to the screen controller! ty :)
         GUIScreenControl.addScreen("login", FXMLLoader.load(getClass().getResource("/login.fxml")));
         GUIScreenControl.addScreen("donorProfile", FXMLLoader.load(getClass().getResource("/donorProfile.fxml")));
