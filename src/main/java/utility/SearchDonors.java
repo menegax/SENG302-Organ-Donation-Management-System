780f17a2374d5ace29ee2d10a4a7bfe7221e72df
Patrick Laffey <plaffey64@gmail.com>
2018-04-20 20:30:54 +1200

    Added removeIndex and clearIndex methods to remove indices

--- src/main/java/utility/SearchDonors.java
+++ src/main/java/utility/SearchDonors.java
@@ -35,7 +35,8 @@ public class SearchDonors {
 
     private static IndexSearcher indexSearcher = null;
 
-    @SuppressWarnings("FieldCanBeLocal")
+    // TODO what is this? it just causes a warning on my machine.
+    //@SuppressWarnings("FieldCanBeLocal")
     private static int NUM_RESULTS = 20;
 
 
@@ -56,11 +57,7 @@ public class SearchDonors {
     public static void createFullIndex() {
         HashSet<Donor> donors = Database.getDonors();
         for (Donor donor : donors) {
-            try {
-                addIndex(donor);
-            } catch (IOException e) {
-                //todo
-            }
+        	addIndex(donor);
         }
     }
 
@@ -68,17 +65,50 @@ public class SearchDonors {
     /**
      * Add indices of donors via the index writer
      * @param donor donor to be indexed
-     * @throws IOException When unable to write the index
      */
-    private static void addIndex(Donor donor) throws IOException {
+    public static void addIndex(Donor donor) {
         if (indexWriter == null) {
-            initializeWriter();
+            try {
+				initializeWriter();
+			} catch (IOException e) {
+				// TODO add error for failure to initialize index writer
+			}
         }
-        indexWriter.commit();
-        indexWriter.addDocument(createDocument(donor));
+        try {
+			indexWriter.commit();
+			indexWriter.addDocument(createDocument(donor));
+		} catch (IOException e) {
+			// TODO add error for failure to write to index
+		}
     }
 
-    // todo add javadoc
+    /**
+     * Removes indices of donors via the index writer
+     * @param donor donor to remove index for
+     */
+    public static void removeIndex(Donor donor) {
+    	Term toDel = new Term("nhi", donor.getNhiNumber());
+    	try {
+			indexWriter.deleteDocuments(toDel);
+		} catch (IOException e) {
+			// TODO add error for failure to delete index
+		}
+    }
+    
+    /**
+     * Removes all indices of all donors via the index writer
+     */
+    public static void clearIndex() {
+    	try {
+			indexWriter.deleteAll();
+		} catch (IOException e) {
+			// TODO add error for failure to delete all indices 
+		}
+    }
+    
+    /**
+     * Creates the index document for a donor
+     */
     private static Document createDocument(Donor donor) {
         Document donorDoc = new Document();
         donorDoc.add(new StringField("nhi", donor.getNhiNumber(), Field.Store.YES));
@@ -87,30 +117,48 @@ public class SearchDonors {
     }
 
 
-    //todo add javadoc
+    /**
+     * Closes the index writer and ram directory freeing up the 
+     * memory back to the operating system
+     * @throws IOException
+     */
     public static void closeIndex() throws IOException {
         ramDirectory.close();
         indexWriter.close();
     }
 
-    //todo add javadoc
+    /**
+     * Searches through the index for any results of the given query
+     * @param query FuzzyQuery object of the query to search for
+     * @return The top 20/value of NUM_RESULTS documents in the index 
+     * @throws IOException If there is an error reading from the index
+     */
     private static TopDocs searchQuery(FuzzyQuery query) throws IOException {
         IndexReader indexReader = DirectoryReader.open(ramDirectory);
         indexSearcher = new IndexSearcher(indexReader);
         return indexSearcher.search(query, NUM_RESULTS);
     }
 
-    //todo add javadoc
-    public static ArrayList<Donor> searchByName(String name) throws IOException {
+    /**
+     * Searches through the index for donors by full name
+     * @param name The name you want to search for
+     * @return ArrayList of the donors it found as a result of the search
+     */
+    public static ArrayList<Donor> searchByName(String name) {
         ArrayList<Donor> results = new ArrayList<>();
         FuzzyQuery fuzzyQuery = new FuzzyQuery(new Term("name", name));
-        TopDocs docs = searchQuery(fuzzyQuery);
-        String nhi;
-        for (ScoreDoc doc : docs.scoreDocs) {
-            Document thisDoc = indexSearcher.doc(doc.doc);
-            nhi = thisDoc.get("nhi");
-            results.add(Database.getDonorByNhi(nhi));
-        }
+        TopDocs docs;
+		try {
+			docs = searchQuery(fuzzyQuery);
+	        String nhi;
+	        for (ScoreDoc doc : docs.scoreDocs) {
+	            Document thisDoc = indexSearcher.doc(doc.doc);
+	            nhi = thisDoc.get("nhi");
+	            results.add(Database.getDonorByNhi(nhi));
+	        }
+		} catch (IOException e) {
+			// TODO add error for unable to find or read from index
+		}
         return results;
     }
 }
