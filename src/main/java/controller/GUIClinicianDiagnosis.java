f84aae872435f2042964b8dc06aa0de49fdfddd4
Maree Palmer <mpa588@uclive.ac.nz>
2018-04-25 22:48:55 +1200

    Created new branch for Story 22. Designed base outline for donor and clinician disease views with necessary nodes. ActionListeners and table filling to be implemented. #story[651]

---
+++ src/main/java/controller/GUIClinicianDiagnosis.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.fxml.FXML;
+
+public class GUIClinicianDiagnosis {
+
+    @FXML
+    public void undo() {
+        System.out.print( "undo" );  // To be completed by Story 12 and 13 responsible's
+    }
+
+    @FXML
+    public void redo() {
+        System.out.print( "redo" );  // To be completed by Story 12 and 13 responsible's
+    }
+}
---
+++ src/main/java/controller/GUIDonorDiagnosis.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.fxml.FXML;
+
+public class GUIDonorDiagnosis {
+
+    @FXML
+    public void undo() {
+        System.out.print( "undo" );  // To be completed by Story 12 and 13 responsible's
+    }
+
+    @FXML
+    public void redo() {
+        System.out.print( "redo" );  // To be completed by Story 12 and 13 responsible's
+    }
+
+}
