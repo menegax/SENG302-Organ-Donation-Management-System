95db34c9dbc2d0b1d7fb8d62258b4f60bcb93e57
Andrew Spearman <asp79@uclive.ac.nz>
2018-05-09 22:56:01 +1200

    Cleaning Main
    - Added JavaDoc
    - Moved methods around for clarity
    
    #story[751] #document #chore #commits[]

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -52,58 +52,6 @@ public class Main extends Application {
     }
 
 
-    private void setUpMenuBar(Stage primaryStage) {
-        if (System.getProperty("os.name").startsWith("Mac")) {
-            setUpMacOsMenuBar(primaryStage);
-        }
-        // if windows, call here...
-    }
-
-
-    /**
-     * Creates a native-looking MacOS menu bar for the application
-     * @param primaryStage the root stage of the application on which to set the menu
-     */
-    private void setUpMacOsMenuBar(Stage primaryStage) {
-
-        String appName = "Big Pharma";
-        // Get the toolkit
-        MenuToolkit tk = MenuToolkit.toolkit();
-
-        // Create a new menu bar
-        MenuBar bar = new MenuBar();
-
-        // Add the default application menu
-        bar.getMenus().add(tk.createDefaultApplicationMenu(appName));
-
-        // Add some more Menus...
-        Menu menu1 = new Menu("App");
-        MenuItem menu1Item1 = new MenuItem("Log out");
-        menu1Item1.setOnAction(event -> System.out.println("Log out clicked"));
-        MenuItem menu1Item2 = tk.createQuitMenuItem(appName);
-        menu1.getItems().addAll(menu1Item1, menu1Item2);
-
-        Menu menu2 = new Menu("File");
-        MenuItem menu2Item1 = new MenuItem("Save");
-        menu2Item1.setOnAction(event -> System.out.println("Save clicked"));
-        MenuItem menu2Item2 = new MenuItem("Import");
-        menu2Item2.setOnAction(event -> System.out.println("Import clicked"));
-        menu2.getItems().addAll(menu2Item1, menu2Item2);
-
-        Menu menu3 = new Menu("Edit");
-        MenuItem menu3Item1 = new MenuItem("Undo");
-        menu3Item1.setOnAction(event -> System.out.println("Undo clicked"));
-        MenuItem menu3Item2 = new MenuItem("Redo");
-        menu3Item2.setOnAction(event -> System.out.println("Redo clicked"));
-        menu3.getItems().addAll(menu3Item1, menu3Item2);
-
-        bar.getMenus().addAll(menu1, menu2, menu3);
-
-        // Use the menu bar for primary stage
-        tk.setMenuBar(primaryStage, bar);
-    }
-
-
     public static void main(String[] args) {
         UserActionHistory.setup(); // start user action logs
         launch(args);
@@ -147,12 +95,82 @@ public class Main extends Application {
 
     }
 
+
+    /**
+     * Adds the default clinician if there isn't one already
+     */
     private void ensureDefaultClinician() {
 
         // if default clinician 0 not in db, add it
         if (!Database.isClinicianInDb(0)) {
-            Database.addClinician(new Clinician(0, "initial", new ArrayList<String>() {{ add("Middle"); }}, "clinician", "Creyke RD", "Ilam RD", "ILAM", GlobalEnums.Region.CANTERBURY));
+            Database.addClinician(new Clinician(0, "initial", new ArrayList<String>() {{
+                add("Middle");
+            }}, "clinician", "Creyke RD", "Ilam RD", "ILAM", GlobalEnums.Region.CANTERBURY));
         }
 
+    }
+
+
+    /**
+     * Sets up the menu bar depending on the OS
+     *
+     * @param primaryStage the stage to set the menu bar to
+     */
+    private void setUpMenuBar(Stage primaryStage) {
+        if (System.getProperty("os.name")
+                .startsWith("Mac")) {
+            setUpMacOsMenuBar(primaryStage);
+        }
+        // if windows, call here...
+    }
+
+
+    /**
+     * Creates a native-looking MacOS menu bar for the application
+     *
+     * @param primaryStage the root stage of the application on which to set the menu
+     */
+    private void setUpMacOsMenuBar(Stage primaryStage) {
+
+        String appName = "Big Pharma";
+        // Get the toolkit
+        MenuToolkit tk = MenuToolkit.toolkit();
+
+        // Create a new menu bar
+        MenuBar bar = new MenuBar();
+
+        // Add the default application menu
+        bar.getMenus()
+                .add(tk.createDefaultApplicationMenu(appName));
+
+        // Add some more Menus...
+        Menu menu1 = new Menu("App");
+        MenuItem menu1Item1 = new MenuItem("Log out");
+        menu1Item1.setOnAction(event -> System.out.println("Log out clicked"));
+        MenuItem menu1Item2 = tk.createQuitMenuItem(appName);
+        menu1.getItems()
+                .addAll(menu1Item1, menu1Item2);
+
+        Menu menu2 = new Menu("File");
+        MenuItem menu2Item1 = new MenuItem("Save");
+        menu2Item1.setOnAction(event -> System.out.println("Save clicked"));
+        MenuItem menu2Item2 = new MenuItem("Import...");
+        menu2Item2.setOnAction(event -> System.out.println("Import clicked"));
+        menu2.getItems()
+                .addAll(menu2Item1, menu2Item2);
+
+        Menu menu3 = new Menu("Edit");
+        MenuItem menu3Item1 = new MenuItem("Undo ⌃Z");
+        menu3Item1.setOnAction(event -> System.out.println("Undo clicked. Soon to be  ⌘⇧Y"));
+        MenuItem menu3Item2 = new MenuItem("Redo ⌃Y");
+        menu3Item2.setOnAction(event -> System.out.println("Redo clicked. Soon to be ⌘Z"));
+        menu3.getItems()
+                .addAll(menu3Item1, menu3Item2);
+
+        bar.getMenus()
+                .addAll(menu1, menu2, menu3);
+
+        // Use the menu bar for primary stage
+        tk.setMenuBar(primaryStage, bar);
     }
 }
