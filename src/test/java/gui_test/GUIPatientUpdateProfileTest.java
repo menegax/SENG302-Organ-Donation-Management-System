a83a2ecd2197ac122f12b4ed37fe0d6e798fbe24
Andrew Spearman <asp79@uclive.ac.nz>
2018-05-11 14:09:58 +1200

    Fixed failings undo / redo tests by fixing a different test
    - A separate test did not close its pop ups after finishing its test
    - The undo/redo would try undo/redo on the pop up instead of the main
    
    #story[764] #fix #test #commits[]

--- src/test/java/gui_test/GUIPatientUpdateProfileTest.java
+++ src/test/java/gui_test/GUIPatientUpdateProfileTest.java
@@ -74,6 +74,8 @@ public class GUIPatientUpdateProfileTest extends ApplicationTest {
         interact(() -> {
             lookup("#nhiTxt").queryAs(TextField.class).setText("999abcd");
             lookup("#saveButton").queryAs(Button.class).fire();
+            lookup("OK").queryAs(Button.class).fire(); // closes alert
+
         });
 
         verifyThat("#patientUpdateAnchorPane", Node::isVisible);
@@ -87,6 +89,8 @@ public class GUIPatientUpdateProfileTest extends ApplicationTest {
         interact(() -> {
             lookup("#nhiTxt").queryAs(TextField.class).setText("TFX9998"); // nhi already in use
             lookup("#saveButton").queryAs(Button.class).fire();
+            lookup("OK").queryAs(Button.class).fire(); // closes alert
+
         });
 
         verifyThat("#patientUpdateAnchorPane", Node::isVisible);
