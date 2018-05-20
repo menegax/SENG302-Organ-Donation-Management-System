6c222a3694860de6f06bd54a555ffaace54f8372
jam357 <jam357@uclive.ac.nz>
2018-05-20 15:17:26 +1200

    #story[702] - Took a test out after realising that receiving organs button isn't accessible to patients #fix

--- src/test/java/gui_test/GUIProfileTest.java
+++ src/test/java/gui_test/GUIProfileTest.java
@@ -89,12 +89,6 @@ public class GUIProfileTest extends ApplicationTest {
         verifyThat("#patientDonationsAnchorPane", Node::isVisible);
     }
 
-    @Test
-    public void should_go_to_receiving_organs() {
-        interact(() -> { lookup("#requirementsButton").queryAs(Button.class).fire(); } );
-        verifyThat("#patientRequirementsAnchorPane", Node::isVisible);
-    }
-
 //    @Test
 //    public void should_have_correct_patient_details() {
 //        // Made around default patient in the system with NHI of ABC1238
