bafc8c737e2205542301e395c21ef26eb9c4f8db
Andrew Spearman <asp79@uclive.ac.nz>
2018-05-21 21:49:08 +1200

    Cleaned up SearchPatientsTest
    - Removing unnecessary code
    - Renamed package Icons to image for consistency
    
    #story[764] #test #chore #commits[]

--- src/test/java/utility_test/SearchPatientsTest.java
+++ src/test/java/utility_test/SearchPatientsTest.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.logging.Level;
 
+import controller.Main;
 import model.Patient;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -18,11 +19,6 @@ import utility.SearchPatients;
 
 public class SearchPatientsTest {
 
-	private static Patient d1;
-	private static Patient d2;
-	private static Patient d3;
-	private static Patient d4;
-	
     /**
      * Populate database with test patients and disables logging
      */
@@ -31,46 +27,36 @@ public class SearchPatientsTest {
 
         userActions.setLevel(Level.OFF);
         Database.resetDatabase();
-        
+
         // Given patients in a db
-        d1 = new Patient("abc1234", "Pat", null, "Laff", LocalDate.now());
-        d2 = new Patient("def1234", "Patik", null, "Laffey", LocalDate.now());
-        d3 = new Patient("ghi1234", "George", null, "Romera", LocalDate.now());
-        d4 = new Patient("jkl1234", "George", null, "Bobington", LocalDate.now());
-        Database.addPatient(d4);
-        Database.addPatient(d3);
-        Database.addPatient(d2);
-        Database.addPatient(d1);
+        Database.addPatient(new Patient("abc1234", "Pat", null, "Laff", LocalDate.now()));
+        Database.addPatient(new Patient("def1234", "Patik", null, "Laffey", LocalDate.now()));
+        Database.addPatient(new Patient("ghi1234", "George", null, "Romera", LocalDate.now()));
+        Database.addPatient(new Patient("jkl1234", "George", null, "Bobington", LocalDate.now()));
 
         // Given an index
-        SearchPatients.createFullIndex();
+        SearchPatients.createFullIndex(); //Todo shouldn't be necessary
     }
+
 
     /**
      * Ensures the search results are changed after a patient's attributes are changed
      *
-     * @throws IOException when the patient's nhi cannot be found in the database
+     * @exception IOException when the patient's nhi cannot be found in the database
      */
     @Test
     public void testSearchAfterUpdatePatient() throws IOException {
 
-            // When first name of patient changed
-            Database.getPatientByNhi("abc1234").setFirstName("Andrew");
+        // Given
 
-            // Then searching by new first name returns correct results
-            ArrayList<Patient> results = SearchPatients.searchByName("Ande Lafey");
+        // When first name of patient changed
+        Database.getPatientByNhi("abc1234")
+                .setFirstName("Andrew");
 
-            assertTrue(results.contains(Database.getPatientByNhi("abc1234")));
-        }
-
-
-    /**
-     * Reset the logging level
-     */
-    @AfterClass
-    public static void tearDown() {
-
-        userActions.setLevel(Level.INFO);
+        // Then searching by new first name returns correct results
+        ArrayList<Patient> results = SearchPatients.searchByName("Ande Lafey");
+        assertTrue(results.contains(Database.getPatientByNhi("abc1234")));
     }
+
 
 }
