b5af8f6170258b8f3b258e5fa4ef639adb1e7525
jam357 <jam357@uclive.ac.nz>
2018-05-23 11:10:12 +1200

    #story[656] - Hiding and showing death date and cured disease if the reason is selected in the deregistration reason choicebox #implement

--- src/main/java/controller/GUIRequiredOrganDeregistrationReason.java
+++ src/main/java/controller/GUIRequiredOrganDeregistrationReason.java
@@ -10,6 +10,7 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import model.Disease;
 import model.Patient;
 import utility.GlobalEnums;
 import java.time.LocalDate;
@@ -42,15 +43,24 @@ public class GUIRequiredOrganDeregistrationReason {
     @FXML
     private Label reasonTitle;
 
+    @FXML
+    private Label curedLabel;
+
+    @FXML
+    private ChoiceBox<Disease> diseaseCured;
+
     private GlobalEnums.Organ organ;
 
     private UserControl userControl;
+
+    private Patient target;
 
     @FXML
     private AnchorPane requiredOrganDeregistrationReasonPane;
 
     public void initialize() {
         userControl = new UserControl();
+        target = userControl.getTargetPatient();
         populateDropdown();
         populateForm();
         dateOfDeath.setDisable(true);
@@ -59,16 +69,28 @@ public class GUIRequiredOrganDeregistrationReason {
         dateOfDeathLabel.setVisible(false);
         okButton1.setDisable(true);
         okButton1.setVisible(false);
+        curedLabel.setDisable(true);
+        curedLabel.setVisible(false);
+        diseaseCured.setDisable(true);
+        diseaseCured.setVisible(false);
     }
 
     /**
      * Populates drop down menu that represent enum data of reasons for deregistering patient required organs
      */
     private void populateDropdown() {
-        // Populate blood group drop down with values from the Blood groups enum
+        // Populate blood group drop down with values from the deregistration reasons enum
         List<GlobalEnums.DeregistrationReason> deregistrationReasons = new ArrayList<>();
-        for (GlobalEnums.DeregistrationReason reason : GlobalEnums.DeregistrationReason.values()) {
-            deregistrationReasons.add(reason);
+        if (target.getCurrentDiseases().size() < 1) {
+            for (GlobalEnums.DeregistrationReason reason : GlobalEnums.DeregistrationReason.values()) {
+                if (reason != GlobalEnums.DeregistrationReason.CURED) {
+                    deregistrationReasons.add(reason);
+                }
+            }
+        } else {
+            for (GlobalEnums.DeregistrationReason reason : GlobalEnums.DeregistrationReason.values()) {
+                deregistrationReasons.add(reason);
+            }
         }
         ObservableList<GlobalEnums.DeregistrationReason> deregistrationReasonsOL = FXCollections.observableList(deregistrationReasons);
         reasons.setItems(deregistrationReasonsOL);
@@ -92,11 +114,45 @@ public class GUIRequiredOrganDeregistrationReason {
     }
 
     /**
+     * set the reason selected
+     */
+    @FXML
+    public void reasonSelected() {
+        if (reasons.getValue() == GlobalEnums.DeregistrationReason.DIED) {
+            curedLabel.setDisable(true);
+            curedLabel.setVisible(false);
+            diseaseCured.setDisable(true);
+            diseaseCured.setVisible(false);
+            dateOfDeath.setDisable(false);
+            dateOfDeath.setVisible(true);
+            dateOfDeathLabel.setDisable(false);
+            dateOfDeathLabel.setVisible(true);
+        } else if (reasons.getValue() == GlobalEnums.DeregistrationReason.CURED) {
+            curedLabel.setDisable(false);
+            curedLabel.setVisible(true);
+            diseaseCured.setDisable(false);
+            diseaseCured.setVisible(true);
+            dateOfDeath.setDisable(true);
+            dateOfDeath.setVisible(false);
+            dateOfDeathLabel.setDisable(true);
+            dateOfDeathLabel.setVisible(false);
+        } else {
+            dateOfDeath.setDisable(true);
+            dateOfDeath.setVisible(false);
+            dateOfDeathLabel.setDisable(true);
+            dateOfDeathLabel.setVisible(false);
+            curedLabel.setDisable(true);
+            curedLabel.setVisible(false);
+            diseaseCured.setDisable(true);
+            diseaseCured.setVisible(false);
+        }
+    }
+
+    /**
      * saves the reason why the clinician removed a organ from the patient required organs list
      */
     public void saveReason() {
         GlobalEnums.DeregistrationReason reason = reasons.getSelectionModel().getSelectedItem();
-        Patient target = userControl.getTargetPatient();
         if (reason == GlobalEnums.DeregistrationReason.ERROR) {
             userActions.log(Level.INFO, "Deregistered " + organ + " due to error", new String[]{"Attempted to deregister " + organ, target.getNhiNumber()});
         } else if (reason == GlobalEnums.DeregistrationReason.CURED) {
