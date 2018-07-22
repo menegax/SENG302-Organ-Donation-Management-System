19f94a8f0e7f15a3d054a9e989de19cdd12e043a
Andrew Spearman <asp79@uclive.ac.nz>
2018-07-23 11:07:48 +1200

    Added GUIHome renaming window title to the target User
    - i.e. "Patient ABC1234" when someone else is viewing the patient
    
    #story[779]

--- src/main/java/controller/GUIHome.java
+++ src/main/java/controller/GUIHome.java
@@ -51,8 +51,11 @@ public class GUIHome implements Observer {
 
     private Stage homeStage;
 
-    /** The user that the home controller is viewing. If it is a clinician viewing a patient it is the patient */
+    /**
+     * The user that the home controller is viewing. If it is a clinician viewing a patient it is the patient
+     */
     private User homeTarget;
+
 
     @FXML
     public void initialize() {
@@ -60,7 +63,7 @@ public class GUIHome implements Observer {
         statusObservable.addObserver(this);
         try {
             // Patient viewing themself
-            if (userControl.getLoggedInUser() instanceof Patient){
+            if (userControl.getLoggedInUser() instanceof Patient) {
                 homeTarget = userControl.getLoggedInUser();
                 addTabsPatient();
                 setUpColouredBar(userControl.getLoggedInUser(), "Patient");
@@ -78,28 +81,39 @@ public class GUIHome implements Observer {
                     addTabsClinician();
                     setUpColouredBar(userControl.getLoggedInUser(), "Clinician");
                 }
-            } else if (userControl.getLoggedInUser() instanceof Administrator) {
+            }
+            else if (userControl.getLoggedInUser() instanceof Administrator) {
+                // admin viewing patient
                 if (userControl.getTargetUser() instanceof Patient) {
+                    homeTarget = userControl.getTargetUser();
                     addTabsForPatientClinician();
                     setUpColouredBar(userControl.getTargetUser(), "Patient");
-                } else if (userControl.getTargetUser() instanceof Clinician) {
+                }
+                // admin viewing clinician
+                else if (userControl.getTargetUser() instanceof Clinician) {
+                    homeTarget = userControl.getTargetUser();
                     addTabsClinicianAdministrator();
                     setUpColouredBar(userControl.getTargetUser(), "Clinician");
-                } else if (userControl.getTargetUser() instanceof Administrator) {
+                }
+                // admin viewing admin
+                else if (userControl.getTargetUser() instanceof Administrator) {
+                    homeTarget = userControl.getTargetUser();
                     addTabsAdministrator();
                     setUpColouredBar(userControl.getTargetUser(), "Administrator");
-                } else {
-                	addTabsAdministrator();
-                	setUpColouredBar(userControl.getLoggedInUser(), "Administrator");
+                }
+                else {
+                    addTabsAdministrator();
+                    setUpColouredBar(userControl.getLoggedInUser(), "Administrator");
                 }
             }
             addStageListener();
-            horizontalTabPane.sceneProperty().addListener((observable, oldScene, newScene) -> newScene.windowProperty().addListener((observable1, oldStage, newStage) -> setUpMenuBar((Stage) newStage)));
-        } catch (IOException e) {
-            e.printStackTrace();
+            horizontalTabPane.sceneProperty()
+                    .addListener((observable, oldScene, newScene) -> newScene.windowProperty()
+                            .addListener((observable1, oldStage, newStage) -> setUpMenuBar((Stage) newStage)));
+        }
+        catch (IOException e) {
             new Alert(ERROR, "Unable to load home").show();
             systemLogger.log(SEVERE, "Failed to load home scene and its fxmls " + e.getMessage());
-            e.printStackTrace();
         }
     }
 
@@ -108,44 +122,62 @@ public class GUIHome implements Observer {
 
         // The following code waits for the stage to be loaded
         if (homePane.getScene() == null) {
-            homePane.sceneProperty().addListener((observable, oldScene, newScene) -> {
-                if (newScene != null) {
-                    if (newScene.getWindow() == null) {
-                        newScene.windowProperty().addListener((observable2, oldStage, newStage) -> {
-                            if (newStage != null) {
-                                homeStage = (Stage) newStage;
+            homePane.sceneProperty()
+                    .addListener((observable, oldScene, newScene) -> {
+                        if (newScene != null) {
+                            if (newScene.getWindow() == null) {
+                                newScene.windowProperty()
+                                        .addListener((observable2, oldStage, newStage) -> {
+                                            if (newStage != null) {
+                                                homeStage = (Stage) newStage;
+                                                // Methods to call after initialize
+                                                setStageTitle();
+                                            }
+                                        });
+                            }
+                            else {
+                                homeStage = (Stage) newScene.getWindow();
                                 // Methods to call after initialize
                                 setStageTitle();
                             }
-                        });
-                    } else {
-                        homeStage = (Stage) newScene.getWindow();
-                        // Methods to call after initialize
-                        setStageTitle();
-                    }
-                }
-            });
-        } else if (homePane.getScene().getWindow() == null){
-            homePane.getScene().windowProperty().addListener((observable2, oldStage, newStage) -> {
-                if (newStage != null) {
-                    homeStage = (Stage) newStage;
-                    // Methods to call after initialize
-                    setStageTitle();
-                }
-            });
-        } else {
-            homeStage = (Stage) homePane.getScene().getWindow();
+                        }
+                    });
+        }
+        else if (homePane.getScene()
+                .getWindow() == null) {
+            homePane.getScene()
+                    .windowProperty()
+                    .addListener((observable2, oldStage, newStage) -> {
+                        if (newStage != null) {
+                            homeStage = (Stage) newStage;
+                            // Methods to call after initialize
+                            setStageTitle();
+                        }
+                    });
+        }
+        else {
+            homeStage = (Stage) homePane.getScene()
+                    .getWindow();
             // Methods to call after initialize
             setStageTitle();
         }
     }
 
+
     private void setStageTitle() {
         homeStage.setTitle("Home");
 
         // If clinician viewing patient
-        if (userControl.getLoggedInUser() instanceof Clinician && userControl.getTargetUser() != null) {
-            homeStage.setTitle("Viewing patient " + ((Patient) homeTarget).getNhiNumber()); //todo change to accomodate admin viewing clinician, admin viewing patient, admin viewing admin
+        if (userControl.getTargetUser() != null) {
+            if (userControl.getTargetUser() instanceof Patient) {
+                homeStage.setTitle("Patient " + ((Patient) homeTarget).getNhiNumber()); //todo fix
+            }
+            else if (userControl.getTargetUser() instanceof Clinician) {
+                homeStage.setTitle("Clinician " + ((Clinician) homeTarget).getStaffID());
+            }
+            else if (userControl.getTargetUser() instanceof Administrator) {
+                homeStage.setTitle("Administrator " + ((Administrator) homeTarget).getUsername());
+            }
         }
 
     }
@@ -153,6 +185,7 @@ public class GUIHome implements Observer {
 
     /**
      * Sets to the coloured bar at top of GUI the user name and type
+     *
      * @param user the currently logged in user, or observed patient
      */
     private void setUpColouredBar(User user, String userType) {
@@ -164,9 +197,11 @@ public class GUIHome implements Observer {
         userTypeDisplay.setText(userType);
     }
 
+
     /**
      * Creates and adds tab to the tab pane
-     * @param title - title of the new tab
+     *
+     * @param title    - title of the new tab
      * @param fxmlPath - path of the fxml to be loaded
      */
     private void createTab(String title, String fxmlPath) throws IOException {
@@ -175,17 +210,21 @@ public class GUIHome implements Observer {
         newTab.setOnSelectionChanged(event -> {
             try {
                 newTab.setContent(FXMLLoader.load(getClass().getResource(fxmlPath)));
-            } catch (IOException e) {
+            }
+            catch (IOException e) {
                 systemLogger.log(SEVERE, "Failed to create tab", e);
             }
         });
         newTab.setContent(FXMLLoader.load(getClass().getResource(fxmlPath)));
-        horizontalTabPane.getTabs().add(newTab);
+        horizontalTabPane.getTabs()
+                .add(newTab);
     }
+
 
     /**
      * Adds tabs to the home tab pane for a patient logged in
-     * @throws IOException - if fxml cannot be located
+     *
+     * @exception IOException - if fxml cannot be located
      */
     private void addTabsPatient() throws IOException {
         createTab("Profile", "/scene/patientProfile.fxml");
@@ -200,9 +239,10 @@ public class GUIHome implements Observer {
 
     /**
      * Adds tabs for a clinician viewing a patient
-     * @throws IOException- if fxml cannot be located
+     *
+     * @exception IOException- if fxml cannot be located
      */
-    private void addTabsForPatientClinician() throws IOException{
+    private void addTabsForPatientClinician() throws IOException {
         createTab("Profile", "/scene/patientProfile.fxml");
         createTab("Update", "/scene/patientUpdateProfile.fxml");
         createTab("Medications", "/scene/patientMedications.fxml");
@@ -216,7 +256,8 @@ public class GUIHome implements Observer {
 
     /**
      * Adds tabs for a logged in clinician
-     * @throws IOException- if fxml cannot be located
+     *
+     * @exception IOException- if fxml cannot be located
      */
     private void addTabsClinician() throws IOException {
         createTab("Profile", "/scene/clinicianProfile.fxml");
@@ -226,9 +267,11 @@ public class GUIHome implements Observer {
         createTab("History", "/scene/clinicianHistory.fxml");
     }
 
+
     /**
      * Adds tabs for a logged in administrator
-     * @throws IOException- if fxml cannot be located
+     *
+     * @exception IOException- if fxml cannot be located
      */
     private void addTabsAdministrator() throws IOException {
         createTab("Profile", "/scene/administratorProfile.fxml");
@@ -239,9 +282,11 @@ public class GUIHome implements Observer {
         createTab("History", "/scene/adminHistory.fxml");
     }
 
+
     /**
      * Adds tabs for an administrator viewing a clinician
-     * @throws IOException- if fxml cannot be located
+     *
+     * @exception IOException- if fxml cannot be located
      */
     private void addTabsClinicianAdministrator() throws IOException {
         createTab("Profile", "/scene/clinicianProfile.fxml");
@@ -256,6 +301,7 @@ public class GUIHome implements Observer {
 
         screenControl.setUpNewLogin(); // ONLY FOR SINGLE USER SUPPORT. REMOVE WHEN MULTI USER SUPPORT
     }
+
 
     /**
      * Creates a native-looking MacOS menu bar for the application
@@ -275,7 +321,8 @@ public class GUIHome implements Observer {
             logOut();
             userActions.log(INFO, "Successfully logged out the user ", "Attempted to log out");
         });
-        menu1.getItems().addAll(menu1Item1);
+        menu1.getItems()
+                .addAll(menu1Item1);
 
         // FILE
         Menu menu2 = new Menu("File");
@@ -304,21 +351,24 @@ public class GUIHome implements Observer {
                     userActions.log(INFO, "Selected clinician file for import", "Attempted to find a file for import");
                 }
             });
-            subMenuImport.getItems().addAll(menu2Item2, menu2Item3);
-            menu2.getItems().addAll(subMenuImport);
+            subMenuImport.getItems()
+                    .addAll(menu2Item2, menu2Item3);
+            menu2.getItems()
+                    .addAll(subMenuImport);
         }
-        menu2.getItems().addAll(menu2Item1);
+        menu2.getItems()
+                .addAll(menu2Item1);
 
         // EDIT
-//        Menu menu3 = new Menu("Edit");
-//        MenuItem menu3Item1 = new MenuItem("Undo");
-//        menu3Item1.setAccelerator(screenControl.getUndo());
-//        menu3Item1.setOnAction(event -> ((UndoableStage) stage).undo());
-//        MenuItem menu3Item2 = new MenuItem("Redo");
-//        menu3Item2.setAccelerator(screenControl.getRedo());
-//        menu3Item2.setOnAction(event -> System.out.println("Redo clicked"));
-//        menu3.getItems()
-//                .addAll(menu3Item1, menu3Item2);
+        //        Menu menu3 = new Menu("Edit");
+        //        MenuItem menu3Item1 = new MenuItem("Undo");
+        //        menu3Item1.setAccelerator(screenControl.getUndo());
+        //        menu3Item1.setOnAction(event -> ((UndoableStage) stage).undo());
+        //        MenuItem menu3Item2 = new MenuItem("Redo");
+        //        menu3Item2.setAccelerator(screenControl.getRedo());
+        //        menu3Item2.setOnAction(event -> System.out.println("Redo clicked"));
+        //        menu3.getItems()
+        //                .addAll(menu3Item1, menu3Item2);
 
         bar.getMenus()
                 .addAll(menu1, menu2);
@@ -349,20 +399,23 @@ public class GUIHome implements Observer {
             }
         }
 
-
     }
+
 
     /**
      * Sets the text of the status label
+     *
      * @param text - The new text
      */
     private void setStatusLbl(String text) {
         statusLbl.setText(text);
     }
 
+
     /**
      * Called when the status text of the StatusObservable is set
-     * @param o The StatusObservable instance
+     *
+     * @param o   The StatusObservable instance
      * @param arg The new status text
      */
     @Override
