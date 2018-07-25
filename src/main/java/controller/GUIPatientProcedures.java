884d4f5b3ebb0a8ce1a7386e19bb8fa4a25cd8f1
Maree Palmer <mpa588@uclive.ac.nz>
2018-07-25 16:52:42 +1200

    Removed popup stage undo/redo call #story[778]

--- src/main/java/controller/GUIPatientProcedures.java
+++ src/main/java/controller/GUIPatientProcedures.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import model.Administrator;
 import model.Clinician;
 import model.Patient;
 import model.Procedure;
@@ -99,7 +100,7 @@ public class GUIPatientProcedures extends UndoableController {
             addProcedureButton.setVisible(false);
             editProcedureButton.setVisible(false);
             deleteProcedureButton.setVisible(false);
-        } else if (userControl.getLoggedInUser() instanceof Clinician) {
+        } else if (userControl.getLoggedInUser() instanceof Clinician || userControl.getLoggedInUser() instanceof Administrator) {
             this.patient = (Patient) userControl.getTargetUser();
             this.patientClone = (Patient) this.patient.deepClone();
             setupTables();
@@ -192,7 +193,7 @@ public class GUIPatientProcedures extends UndoableController {
     public void addProcedure() {
         try {
             UndoableStage stage = new UndoableStage();
-            stage.setPopUp();
+//            stage.setPopUp();
             screenControl.addStage(stage.getUUID(), stage);
             screenControl.show(stage.getUUID(),FXMLLoader.load(getClass().getResource("/scene/patientProcedureForm.fxml")));
             stage.setOnHiding(event -> Platform.runLater(this::tableRefresh));
@@ -223,7 +224,7 @@ public class GUIPatientProcedures extends UndoableController {
         }
         try {
             UndoableStage stage = new UndoableStage();
-            stage.setPopUp();
+//            stage.setPopUp();
             screenControl.addStage(stage.getUUID(), stage);
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/scene/patientProcedureForm.fxml"));
             screenControl.show(stage.getUUID(),fxmlLoader.load());
--- src/main/java/utility/undoRedo/UndoableStage.java
+++ src/main/java/utility/undoRedo/UndoableStage.java
@@ -136,18 +136,18 @@ public class UndoableStage extends Stage {
         }
     }
 
-    /**
-     * Adds listeners for undo/redo for undoable pop-ups (procedures, diagnoses)
-     */
-    public void setPopUp() {
-        this.getScene().setOnKeyPressed(event ->  {
-            if (screenControl.getUndo().match(event)) {
-                undo();
-            } else if (screenControl.getRedo().match(event)) {
-                redo();
-            }
-        });
-    }
+//    /**
+//     * Adds listeners for undo/redo for undoable pop-ups (procedures, diagnoses)
+//     */
+//    public void setPopUp() {
+//        this.getScene().setOnKeyPressed(event ->  {
+//            if (screenControl.getUndo().match(event)) {
+//                undo();
+//            } else if (screenControl.getRedo().match(event)) {
+//                redo();
+//            }
+//        });
+//    }
 
     /**
      * Gets the UUID of the stage
