3ee927d9aa37338ddffbfe84192186133203af04
Andrew Spearman <asp79@uclive.ac.nz>
2018-08-03 14:16:26 +1200

    Implemented future threading for Google API requests
    - Merged thread pool class from DB2 branch
    
    #story[1000] #pair[asp79,hta55]

--- src/main/java/service/APIGoogleMaps.java
+++ src/main/java/service/APIGoogleMaps.java
@@ -7,10 +7,14 @@ import com.google.maps.GeocodingApi;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.GeocodingResult;
 import com.google.maps.model.LatLng;
+import utility.CachedThreadPool;
 import utility.SystemLogger;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.logging.Level;
 
 public class APIGoogleMaps {
@@ -24,11 +28,13 @@ public class APIGoogleMaps {
     private static GeoApiContext context = new GeoApiContext.Builder().apiKey(placeApiKey)
             .build();
 
+
     private APIGoogleMaps() {
         //todo make into proper singleton
     }
 
-    public static void main(String[] ars) throws InterruptedException, ApiException, IOException {
+
+    public static void main(String[] ars) throws InterruptedException, ApiException, IOException, ExecutionException {
 
         GeocodingResult[] results = GeocodingApi.geocode(context, "1600 Amphitheatre Parkway Mountain View, CA 94043")
                 .await();
@@ -41,18 +47,31 @@ public class APIGoogleMaps {
         System.out.println("Lat: " + lat);
         System.out.println("Lng: " + lng);
         System.out.println("Location: " + results[0].geometry.location);
+
+        System.out.println("SUP: " + getLatLng("hey"));
     }
+
 
     public APIGoogleMaps getApiGoogleMaps() {
         return apiGoogleMaps;
     }
 
-    public LatLng getLatLng(String address) throws InterruptedException, ApiException, IOException {
+
+    public static LatLng getLatLng(String address) throws InterruptedException, ExecutionException {
         //todo should this be caught? or method thrown?
 
-        GeocodingResult[] results = GeocodingApi.geocode(context, "1600 Amphitheatre Parkway Mountain View, CA 94043").await();
-        return results[0].geometry.location;
+        CachedThreadPool pool = CachedThreadPool.getCachedThreadPool();
 
+        Callable<LatLng> task = () -> {
+            GeocodingResult[] results = GeocodingApi.geocode(context, "1600 Amphitheatre Parkway Mountain View, CA 94043")
+                    .await();
+            return results[0].geometry.location;
+
+        };
+
+        Future<LatLng> results = pool.getThreadService().submit(task);
+
+        return results.get();
     }
 
 }
