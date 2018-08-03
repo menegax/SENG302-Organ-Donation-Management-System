a89254401df3c1563e967db4257e8a7109cef3e4
Andrew Spearman <asp79@uclive.ac.nz>
2018-08-03 13:43:30 +1200

    Implemented Google Maps API method to get latLng from address string
    #story[1000] #pair[asp79,jam357]

--- src/main/java/service/APIGoogleMaps.java
+++ src/main/java/service/APIGoogleMaps.java
@@ -6,8 +6,12 @@ import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.LatLng;
+import utility.SystemLogger;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
 
 public class APIGoogleMaps {
 
@@ -17,22 +21,38 @@ public class APIGoogleMaps {
 
     private static String originalApiKey = "AIzaSyB9o0cMPFkqyKOTyifZYMiOdlMXUyMgpzA";
 
-    private APIGoogleMaps() throws InterruptedException, ApiException, IOException {
+    private static GeoApiContext context = new GeoApiContext.Builder().apiKey(placeApiKey)
+            .build();
 
+    private APIGoogleMaps() {
+        //todo make into proper singleton
     }
 
     public static void main(String[] ars) throws InterruptedException, ApiException, IOException {
-        GeoApiContext context = new GeoApiContext.Builder().apiKey(placeApiKey)
-                .build();
+
         GeocodingResult[] results = GeocodingApi.geocode(context, "1600 Amphitheatre Parkway Mountain View, CA 94043")
                 .await();
         Gson gson = new GsonBuilder().setPrettyPrinting()
                 .create();
-        System.out.println(gson.toJson(results[0].addressComponents));
+
+        double lat = results[0].geometry.location.lat;
+        double lng = results[0].geometry.location.lng;
+
+        System.out.println("Lat: " + lat);
+        System.out.println("Lng: " + lng);
+        System.out.println("Location: " + results[0].geometry.location);
     }
 
     public APIGoogleMaps getApiGoogleMaps() {
         return apiGoogleMaps;
+    }
+
+    public LatLng getLatLng(String address) throws InterruptedException, ApiException, IOException {
+        //todo should this be caught? or method thrown?
+
+        GeocodingResult[] results = GeocodingApi.geocode(context, "1600 Amphitheatre Parkway Mountain View, CA 94043").await();
+        return results[0].geometry.location;
+
     }
 
 }
