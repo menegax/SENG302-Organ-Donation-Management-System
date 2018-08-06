236b6b9a071fb08f496f94fb467353d95ab7bbfb
Kyle <kla98@uclive.ac.nz>
2018-08-07 11:33:26 +1200

    Added javadoc to admin and user Dao interfaces #story[931]

--- src/main/java/DataAccess/interfaces/IUserDataAccess.java
+++ src/main/java/DataAccess/interfaces/IUserDataAccess.java
@@ -8,13 +8,39 @@ import java.util.Set;
 
 public interface IUserDataAccess {
 
+    /**
+     * Adds a user to the local database. It is added to the appropriate set (patients, clinician, administrators)
+     * based on the type of the user
+     * @param user The user to add
+     */
     void addUser(User user);
 
+    /**
+     * Deletes the given user from its associated set in the database (patients, clinicians, administrators)
+     * and adds the user to the deleted users set
+     * @param user The user to delete
+     */
     void deleteUser(User user);
 
+    /**
+     * Returns all users in the local database
+     * @return
+     */
     Set<User> getUsers();
 
+    /**
+     * Returns a set of users that have been deleted in the local database.
+     * @return the deleted users set
+     */
     Set<User> getDeletedUsers();
 
+    /**
+     * Searches the users in the local database using the given search term and returns matching results.
+     * If the searchTerm is the empty string, all results are matched,
+     * else a match is determined by if the identifier, any of the names, or the full name
+     * is within 2 characters difference (Using the levenshtein distance) from the search term
+     * @param searchTerm The search term to match users on
+     * @return
+     */
     Map<Integer, List<User>> searchUsers(String searchTerm);
 }
