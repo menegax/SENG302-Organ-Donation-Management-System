c560f1650982d74f5b498ad12f6c53fcd60c246e
Kyle <kla98@uclive.ac.nz>
2018-08-07 11:52:53 +1200

    Added javadoc to contact and transplant waiting list Dao interfaces #story[931]

--- src/main/java/DataAccess/interfaces/IContactDataAccess.java
+++ src/main/java/DataAccess/interfaces/IContactDataAccess.java
@@ -7,22 +7,25 @@ import java.util.List;
 public interface IContactDataAccess {
 
     /**
+     * Updates the contact details for the given patient
      *
-     * @param patient -
-     * @return -
+     * @param patient The patients to update
+     * @return The success code of the update
      */
     boolean updateContact(Patient patient);
 
     /**
+     * Returns the contact details of the patient with the given nhi
      *
-     * @param nhi -
-     * @return -
+     * @param nhi The patients nhi to fetch contact details from
+     * @return The List of strings representing the contact details
      */
     List<String> getContactByNhi(String nhi);
 
     /**
+     * Deletes contact details for the patient with the given nhi
      *
-     * @return -
+     * @param nhi The patients nhi to delete contact details from
      */
-    public void deleteContactByNhi(String nhi);
+    void deleteContactByNhi(String nhi);
 }
