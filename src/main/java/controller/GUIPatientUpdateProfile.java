033817091e87cb97767226ed9b31a43f9f60a51c
Andrew Spearman <asp79@uclive.ac.nz>
2018-08-11 20:03:50 +1200

    Implemented correct input validation for patient death and location
    - Refactored validation to be more legible and maintainable
    
    #story[1000]

--- src/main/java/controller/GUIPatientUpdateProfile.java
+++ src/main/java/controller/GUIPatientUpdateProfile.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.GridPane;
 import model.Patient;
 import service.PatientDataService;
 import tornadofx.control.DateTimePicker;
+import utility.GlobalEnums;
 import utility.GlobalEnums.*;
 import utility.undoRedo.Action;
 import utility.undoRedo.StatesHistoryScreen;
@@ -97,8 +98,6 @@ public class GUIPatientUpdateProfile extends UndoableController {
     private Patient target;
 
     private Patient after;
-
-    private UserControl userControl;
 
     private PatientDataService patientDataService = new PatientDataService();
 
@@ -264,189 +263,26 @@ public class GUIPatientUpdateProfile extends UndoableController {
     }
 
     /**
-     * Checks for invalidity of a double used for height or weight.
-     * Returns true if input is not a valid double or the input is a valid double with a value of less than 0.
-     *
-     * @param input String input from text field
-     * @return boolean is invalid
-     */
-    private boolean isInvalidDouble(String input) {
-        try {
-            double value = Double.parseDouble(input);
-            return (value < 0);
-        } catch (NumberFormatException e) {
-            return true;
-        }
-    }
-
-    /**
      * Sets profile changes after checking each field for validity
      */
     @FXML
     public void saveProfileUpdater() {
-        systemLogger.log(Level.FINEST, "Starting patient update method...");
         Boolean valid = true;
-
         StringBuilder invalidContent = new StringBuilder();
 
-        // nhi
-        if (!Pattern.matches(UIRegex.NHI.getValue(), nhiTxt.getText().toUpperCase())) {
-            valid = setInvalid(nhiTxt);
-            invalidContent.append("NHI must be three letters followed by four numbers. ");
-        }
-
-
-        // if the nhi in use doesn't belong to the logged in patient already then it must be taken by someone else
-        if (!patientDataService.getPatientByNhi(nhiTxt.getText()).getNhiNumber().equals(target.getNhiNumber())) {
-            valid = setInvalid(nhiTxt);
-            invalidContent.append("NHI is already in use\n");
-        } else {
-            setValid(nhiTxt);
-        }
-
-
-        // first name
-        if (!Pattern.matches(UIRegex.FNAME.getValue(), firstnameTxt.getText())) {
-            valid = setInvalid(firstnameTxt);
-            invalidContent.append("First name must be letters, ., or -. ");
-        } else {
-            setValid(firstnameTxt);
-        }
-
-        // last name
-        if (!Pattern.matches(UIRegex.LNAME.getValue(), lastnameTxt.getText())) {
-            valid = setInvalid(lastnameTxt);
-            invalidContent.append("Last name must be letters, ., or -. ");
-        } else {
-            setValid(lastnameTxt);
-        }
-
-        //middle names
-        if (!Pattern.matches(UIRegex.MNAME.getValue(), middlenameTxt.getText())) {
-            valid = setInvalid(middlenameTxt);
-            invalidContent.append("Middle name(s) must be letters, ., or -.");
-        } else {
-            setValid(middlenameTxt);
-        }
-
-        // preferred name
-        if (!Pattern.matches(UIRegex.FNAME.getValue(), preferrednameTxt.getText())) {
-            valid = setInvalid(preferrednameTxt);
-        } else {
-            setValid(preferrednameTxt);
-        }
-
-        // region
-        if (regionDD.getSelectionModel()
-                .getSelectedIndex() != -1) {
-            Enum region = Region.getEnumFromString(regionDD.getSelectionModel()
-                    .getSelectedItem());
-            if (region == null) {
-                valid = setInvalid(regionDD);
-                invalidContent.append("Region must be a valid selection from the dropdown. ");
-            } else {
-                setValid(regionDD);
-            }
-        } else {
-            setValid(regionDD);
-        }
-
-
-        // zip
-        if (!zipTxt.getText().equals("")) {
-            try {
-                if (!Pattern.matches(UIRegex.ZIP.getValue(), zipTxt.getText())) {
-                    valid = setInvalid(zipTxt);
-                    invalidContent.append("Zip must be four digits. ");
-                } else {
-                    Integer.parseInt(zipTxt.getText());
-                    setValid(zipTxt);
-                }
-            } catch (NumberFormatException e) {
-                valid = setInvalid(zipTxt);
-                invalidContent.append("Zip must be four digits. ");
-            }
-        } else {
-            setValid(zipTxt);
-        }
-
-
-        // weight
-        if (!Pattern.matches(UIRegex.WEIGHT.getValue(), weightTxt.getText())) {
-            valid = setInvalid(weightTxt);
-            invalidContent.append("Weight must be a valid decimal number\n");
-        } else {
-            setValid(weightTxt);
-        }
-
-
-        // height
-        if (!Pattern.matches(UIRegex.HEIGHT.getValue(), heightTxt.getText())) {
-            valid = setInvalid(heightTxt);
-            invalidContent.append("Height must be a valid decimal number\n");
-        } else {
-            setValid(heightTxt);
-        }
-
-
-        // blood group
-        if (bloodGroupDD.getValue() != null) {
-            String bgStr = bloodGroupDD.getValue();
-            Enum bloodgroup = BloodGroup.getEnumFromString(bgStr);
-            if (bloodgroup == null) {
-                valid = setInvalid(bloodGroupDD);
-                invalidContent.append("Blood group must be a valid selection. ");
-            } else {
-                setValid(bloodGroupDD);
-            }
-        } else {
-            setValid(bloodGroupDD);
-        }
-
-        // date of birth
-        if (dobDate.getValue() != null) {
-            if (dobDate.getValue().isAfter(LocalDate.now())) {
-                valid = setInvalid(dobDate);
-                invalidContent.append("Date of birth must be a valid date either today or earlier and must be before date of death. ");
-            } else {
-                setValid(dobDate);
-            }
-        } else {
-            valid = setInvalid(dobDate);
-        }
-
-        // date of death
-        if (dateOfDeath.getValue() != null) {
-            if ((dobDate.getValue() != null && dateOfDeath.getValue()
-                    .isBefore(dobDate.getValue())) || dateOfDeath.getValue()
-                    .isAfter(LocalDate.now())) {
-                valid = setInvalid(dateOfDeath);
-                invalidContent.append("Date of death must be a valid date either today or earlier and must be after date of birth. ");
-            } else {
-                setValid(dateOfDeath);
-            }
-        } else {
-            if (deathLocationTxt.getText() != null) {
-                valid = setInvalid(dateOfDeath);
-                invalidContent.append("Death date required if death location set");
-            }
-            setValid(dateOfDeath);
-        }
-
-        // death location
-        if (dateOfDeath.getValue() != null) {
-            if (deathLocationTxt.getText().length() == 0) {
-                valid = setInvalid(deathLocationTxt);
-                invalidContent.append("Death location required if death date set");
-            } else if (!deathLocationTxt.getText().matches(UIRegex.DEATH_LOCATION.getValue())) {
-                valid = setInvalid(deathLocationTxt);
-                invalidContent.append("Incorrect death location format");
-            } else {
-                setValid(deathLocationTxt);
-            }
-        } else {
-            setValid(deathLocationTxt);
-        }
+        valid = validateNhi(valid, invalidContent);
+        valid = validateFirstName(valid, invalidContent);
+        valid = validateLastName(valid, invalidContent);
+        valid = validateMiddleNames(valid, invalidContent);
+        valid = validatePreferredName(valid);
+        valid = validateRegion(valid, invalidContent);
+        valid = validateZip(valid, invalidContent);
+        valid = validateWeight(valid, invalidContent);
+        valid = validateHeight(valid, invalidContent);
+        valid = validateBloodGroup(valid, invalidContent);
+        valid = validateBirthDate(valid, invalidContent);
+        valid = validateDeathDate(valid, invalidContent);
+        valid = validateDeathLocation(valid, invalidContent);
 
         // if all are valid
         if (valid) {
@@ -518,6 +354,215 @@ public class GUIPatientUpdateProfile extends UndoableController {
             userActions.log(Level.WARNING, invalidContent.toString(), new String[]{"Attempted to update patient profile", after.getNhiNumber()});
         }
     }
+
+
+    private Boolean validateDeathLocation(Boolean valid, StringBuilder invalidContent) {
+        // death location
+        if (deathLocationTxt.getText().length() != 0) {
+            if (!deathLocationTxt.getText().matches(UIRegex.DEATH_LOCATION.getValue())) {
+                valid = setInvalid(deathLocationTxt);
+                invalidContent.append("Incorrect death location format. ");
+            } else {
+                setValid(deathLocationTxt);
+            }
+        }
+        else if (dateOfDeath.getValue() != null) {
+            // make sure if deathLoc set, that deathDate is also set otherwise deathLoc is invalid
+            valid = setInvalid(deathLocationTxt);
+            invalidContent.append("Death location required if death date set. ");
+        } else {
+            setValid(deathLocationTxt);
+        }
+        return valid;
+    }
+
+
+    private Boolean validateDeathDate(Boolean valid, StringBuilder invalidContent) {
+        // date of death
+        if (dateOfDeath.getValue() != null) {
+            if ((dobDate.getValue() != null && dateOfDeath.getValue()
+                    .isBefore(dobDate.getValue())) || dateOfDeath.getValue()
+                    .isAfter(LocalDate.now())) {
+                valid = setInvalid(dateOfDeath);
+                invalidContent.append("Date of death must be a valid date either today or earlier and must be after date of birth. ");
+            } else {
+                setValid(dateOfDeath);
+            }
+        }
+        else if (deathLocationTxt.getText().length() != 0) {
+            // make sure if deathDate set, that deathLoc also set otherwise deathDate invalid
+            valid = setInvalid(dateOfDeath);
+            invalidContent.append("Death date required if death location set. ");
+        } else {
+            setValid(dateOfDeath);
+        }
+        return valid;
+    }
+
+
+    private Boolean validateBirthDate(Boolean valid, StringBuilder invalidContent) {
+        // date of birth
+        if (dobDate.getValue() != null) {
+            if (dobDate.getValue().isAfter(LocalDate.now())) {
+                valid = setInvalid(dobDate);
+                invalidContent.append("Date of birth must be a valid date either today or earlier and must be before date of death. ");
+            } else {
+                setValid(dobDate);
+            }
+        } else {
+            valid = setInvalid(dobDate);
+        }
+        return valid;
+    }
+
+
+    private Boolean validateBloodGroup(Boolean valid, StringBuilder invalidContent) {
+        // blood group
+        if (bloodGroupDD.getValue() != null) {
+            String bgStr = bloodGroupDD.getValue();
+            Enum bloodgroup = BloodGroup.getEnumFromString(bgStr);
+            if (bloodgroup == null) {
+                valid = setInvalid(bloodGroupDD);
+                invalidContent.append("Blood group must be a valid selection. ");
+            } else {
+                setValid(bloodGroupDD);
+            }
+        } else {
+            setValid(bloodGroupDD);
+        }
+        return valid;
+    }
+
+
+    private Boolean validateHeight(Boolean valid, StringBuilder invalidContent) {
+        // height
+        if (!Pattern.matches(UIRegex.HEIGHT.getValue(), heightTxt.getText())) {
+            valid = setInvalid(heightTxt);
+            invalidContent.append("Height must be a valid decimal number. ");
+        } else {
+            setValid(heightTxt);
+        }
+        return valid;
+    }
+
+
+    private Boolean validateWeight(Boolean valid, StringBuilder invalidContent) {
+        // weight
+        if (!Pattern.matches(UIRegex.WEIGHT.getValue(), weightTxt.getText())) {
+            valid = setInvalid(weightTxt);
+            invalidContent.append("Weight must be a valid decimal number. ");
+        } else {
+            setValid(weightTxt);
+        }
+        return valid;
+    }
+
+
+    private Boolean validateZip(Boolean valid, StringBuilder invalidContent) {
+        // zip
+        if (!zipTxt.getText().equals("")) {
+            try {
+                if (!Pattern.matches(UIRegex.ZIP.getValue(), zipTxt.getText())) {
+                    valid = setInvalid(zipTxt);
+                    invalidContent.append("Zip must be four digits. ");
+                } else {
+                    Integer.parseInt(zipTxt.getText());
+                    setValid(zipTxt);
+                }
+            } catch (NumberFormatException e) {
+                valid = setInvalid(zipTxt);
+                invalidContent.append("Zip must be four digits. ");
+            }
+        } else {
+            setValid(zipTxt);
+        }
+        return valid;
+    }
+
+
+    private Boolean validateRegion(Boolean valid, StringBuilder invalidContent) {
+        // region
+        if (regionDD.getSelectionModel()
+                .getSelectedIndex() != -1) {
+            Enum region = Region.getEnumFromString(regionDD.getSelectionModel()
+                    .getSelectedItem());
+            if (region == null) {
+                valid = setInvalid(regionDD);
+                invalidContent.append("Region must be a valid selection from the dropdown. ");
+            } else {
+                setValid(regionDD);
+            }
+        } else {
+            setValid(regionDD);
+        }
+        return valid;
+    }
+
+
+    private Boolean validatePreferredName(Boolean valid) {
+        // preferred name
+        if (!Pattern.matches(UIRegex.FNAME.getValue(), preferrednameTxt.getText())) {
+            valid = setInvalid(preferrednameTxt);
+        } else {
+            setValid(preferrednameTxt);
+        }
+        return valid;
+    }
+
+
+    private Boolean validateMiddleNames(Boolean valid, StringBuilder invalidContent) {
+        //middle names
+        if (!Pattern.matches(UIRegex.MNAME.getValue(), middlenameTxt.getText())) {
+            valid = setInvalid(middlenameTxt);
+            invalidContent.append("Middle name(s) must be letters, ., or -.");
+        } else {
+            setValid(middlenameTxt);
+        }
+        return valid;
+    }
+
+
+    private Boolean validateLastName(Boolean valid, StringBuilder invalidContent) {
+        // last name
+        if (!Pattern.matches(UIRegex.LNAME.getValue(), lastnameTxt.getText())) {
+            valid = setInvalid(lastnameTxt);
+            invalidContent.append("Last name must be letters, ., or -. ");
+        } else {
+            setValid(lastnameTxt);
+        }
+        return valid;
+    }
+
+
+    private Boolean validateFirstName(Boolean valid, StringBuilder invalidContent) {
+        // first name
+        if (!Pattern.matches(UIRegex.FNAME.getValue(), firstnameTxt.getText())) {
+            valid = setInvalid(firstnameTxt);
+            invalidContent.append("First name must be letters, ., or -. ");
+        } else {
+            setValid(firstnameTxt);
+        }
+        return valid;
+    }
+
+
+    private Boolean validateNhi(Boolean valid, StringBuilder invalidContent) {
+        // nhi
+        if (!Pattern.matches(UIRegex.NHI.getValue(), nhiTxt.getText().toUpperCase())) {
+            valid = setInvalid(nhiTxt);
+            invalidContent.append("NHI must be three letters followed by four numbers. ");
+        }
+
+        // if the nhi in use doesn't belong to the logged in patient already then it must be taken by someone else
+        if (!patientDataService.getPatientByNhi(nhiTxt.getText()).getNhiNumber().equals(target.getNhiNumber())) {
+            valid = setInvalid(nhiTxt);
+            invalidContent.append("NHI is already in use. ");
+        } else {
+            setValid(nhiTxt);
+        }
+        return valid;
+    }
+
 
     /***
      * Applies the invalid class to the target control
