9d228519d630578d273fbef52cb0f7dacad8d1cf
Kyle <kla98@uclive.ac.nz>
2018-08-15 21:33:11 +1200

    Merged in dev #story[1056]

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,8 +13,6 @@ public class App {
     public static void main(String[] argv) {
 
         List<String> argArrayList = Arrays.asList(argv);
-        System.setProperty("connection_type", GlobalEnums.DbType.PRODUCTION.getValue()); //LEAVE HERE!! production db
-
 
         System.setProperty("connection_type", GlobalEnums.DbType.PRODUCTION.getValue()); //LEAVE HERE!! production db
 
