b2c26149023fc9ec01ce898598201eb0dd34a39e
Andrew Spearman <asp79@uclive.ac.nz>
2018-08-15 16:15:10 +1200

    Clicking marker details button now opens patient profile
    #story[1056]

--- src/main/java/utility/JSInjector.java
+++ src/main/java/utility/JSInjector.java
@@ -1,28 +1,19 @@
 package utility;
 
+import controller.GUIHome;
 import controller.ScreenControl;
+import model.Patient;
+import service.PatientDataService;
 
-
-
-//todo remove before review as currently unneeded. (Leaving it in for now incase it is needed later)
 public class JSInjector {
 
     private ScreenControl screenControl = ScreenControl.getScreenControl();
 
-//    public List<Patient> getPatients() {
-//        List<Patient> patients = new ArrayList<>(new ClinicianDataService().searchPatients("", null, 30));
-//        List<Patient> results = new ArrayList<>();
-//        for (Patient p : patients) {
-//            results.add(new PatientDataService().getPatientByNhi(p.getNhiNumber()));
-//        }
-//        return results;
-//    }
-
+    @SuppressWarnings("unused") // used in js
     public void openPatientProfile(String patientNhi) {
-        System.out.println("YAY");
-//        Patient patient = new PatientDataService().getPatientByNhi(patientNhi);
-//        GUIHome controller = (GUIHome) screenControl.show("/scene/home.fxml", true, null, patient);
-//        controller.setTarget(patient);
+        Patient patient = new PatientDataService().getPatientByNhi(patientNhi);
+        GUIHome controller = (GUIHome) screenControl.show("/scene/home.fxml", true, null, patient);
+        controller.setTarget(patient);
     }
 
 }
