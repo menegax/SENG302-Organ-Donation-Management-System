1efb02cd142be5b9def1bce94c656d21f8b2a7e9
Andrew Spearman <asp79@uclive.ac.nz>
2018-09-21 16:23:44 +1200

    Added custom icons to markers
    #story[1114]

--- src/main/resources/html/map.js
+++ src/main/resources/html/map.js
@@ -5,22 +5,18 @@ var validCount = 0;
 
 function init() {
     geocoder = new google.maps.Geocoder();
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -40.59225, lng: 173.51012},
-            zoom: 6,
-            disableDefaultUI: true,
-            scaleControl: true,
-            gestureHandling: 'cooperative'
-        });
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -40.59225, lng: 173.51012}, zoom: 6, disableDefaultUI: true, scaleControl: true, gestureHandling: 'cooperative'
+    });
 
-    google.maps.event.addListenerOnce(map, 'idle', function(){
+    google.maps.event.addListenerOnce(map, 'idle', function () {
         setMapDragEnd();
         markerLoop(patients.size());
 
-        document.getElementById('availableOrgansView').addEventListener('click', function() {
+        document.getElementById('availableOrgansView').addEventListener('click', function () {
             validCount = 0;
 
-            markers.forEach(function(marker) {
+            markers.forEach(function (marker) {
                 marker.setMap(null);
             });
             markers = [];
@@ -37,38 +33,45 @@ function init() {
  */
 function setMapDragEnd() {
     // Bounds for the World
-    var allowedBounds = new google.maps.LatLngBounds(
-        new google.maps.LatLng(-84.220892, -177.871399),
-        new google.maps.LatLng(84.889374, 179.872535));
+    var allowedBounds = new google.maps.LatLngBounds(new google.maps.LatLng(-84.220892, -177.871399), new google.maps.LatLng(84.889374, 179.872535));
 
     // Listen for the dragend event
-    google.maps.event.addListener(map, 'dragend', function() {
-        if (allowedBounds.contains(map.getCenter())) return;
+    google.maps.event.addListener(map, 'dragend', function () {
+        if (allowedBounds.contains(map.getCenter())) {
+            return;
+        }
 
         // Out of bounds - Move the map back within the bounds
 
-        var c = map.getCenter(),
-            x = c.lng(),
-            y = c.lat(),
-            maxX = allowedBounds.getNorthEast().lng(),
-            maxY = allowedBounds.getNorthEast().lat(),
-            minX = allowedBounds.getSouthWest().lng(),
-            minY = allowedBounds.getSouthWest().lat();
+        var c = map.getCenter(), x = c.lng(), y = c.lat(), maxX = allowedBounds.getNorthEast().lng(), maxY = allowedBounds.getNorthEast().lat(),
+                minX = allowedBounds.getSouthWest().lng(), minY = allowedBounds.getSouthWest().lat();
 
-        if (x < minX) x = minX;
-        if (x > maxX) x = maxX;
-        if (y < minY) y = minY;
-        if (y > maxY) y = maxY;
+        if (x < minX) {
+            x = minX;
+        }
+        if (x > maxX) {
+            x = maxX;
+        }
+        if (y < minY) {
+            y = minY;
+        }
+        if (y > maxY) {
+            y = maxY;
+        }
 
         map.setCenter(new google.maps.LatLng(y, x));
     });
 }
 
 function markerLoop(i) {
-    if (i < 1) return;
-    if (validCount >= 30) return;
-    addMarker(patients.get(i-1));
-    setTimeout(function() {
+    if (i < 1) {
+        return;
+    }
+    if (validCount >= 30) {
+        return;
+    }
+    addMarker(patients.get(i - 1));
+    setTimeout(function () {
         markerLoop(--i);
     }, 700);
 }
@@ -85,7 +88,8 @@ function addMarker(patient) {
         var marker = makeMarker(patient, latLong); //set up markers
         attachInfoWindow(patient, marker);
         markers.push(marker);
-    } else {
+    }
+    else {
         console.log('Geocoding failed because: ' + status);
     }
 }
@@ -95,18 +99,28 @@ function makeMarker(patient, results) {
 
     var randx = Math.random() * 0.02 - 0.01;
     var randy = Math.random() * 0.02 - 0.01;
-    var finalLoc = new google.maps.LatLng(results.lat + randx, results.lng + randy); //todo replace with patient.getCurrentLocation
+    var finalLoc = new google.maps.LatLng(results.lat + randx, results.lng + randy);
 
-    if (patient.isDead()){
+    if (patient.isDead()) {
         return new google.maps.Marker({
-            map: map, position: finalLoc, title: name, animation: google.maps.Animation.DROP, label: 'D'
-        });
-    } else if (!patient.isDead()) {
-        return new google.maps.Marker({
-            map: map, position: finalLoc, title: name, animation: google.maps.Animation.DROP, label: 'A'
+            map: map,
+            position: finalLoc,
+            title: name,
+            animation: google.maps.Animation.DROP,
+            label: 'D',
+            icon: 'https://maps.google.com/mapfiles/kml/shapes/info-i_maps.png'
         });
     }
-
+    else if (!patient.isDead()) {
+        return new google.maps.Marker({
+            map: map,
+            position: finalLoc,
+            title: name,
+            animation: google.maps.Animation.DROP,
+            label: 'A',
+            icon: 'https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png'
+        });
+    }
 
 }
 
@@ -115,22 +129,21 @@ function attachInfoWindow(patient, marker) {
     var organOptions = getOrganOptions(patient);
 
     var infoWindow = new google.maps.InfoWindow({
-        content: '<h5>' + patient.getNhiNumber() + ' - ' + patient.getNameConcatenated() +
-        '</h5><span style="font-size: 14px">' +
-        patient.getAddressString() + '<br><br>' +
-        organOptions.donating + '<br><br>' +
-        organOptions.receiving +
-        '</span><br><input type="button" onclick="openPatientProfile(\''+patient.getNhiNumber()+'\')" class="btn btn-sm btn-primary mt-3" style="margin: auto" value="Open Profile"/>'
+        content: '<h5>' + patient.getNhiNumber() + ' - ' + patient.getNameConcatenated() + '</h5><span style="font-size: 14px">'
+        + patient.getAddressString() + '<br><br>' + organOptions.donating + '<br><br>' + organOptions.receiving
+        + '</span><br><input type="button" onclick="openPatientProfile(\'' + patient.getNhiNumber()
+        + '\')" class="btn btn-sm btn-primary mt-3" style="margin: auto" value="Open Profile"/>'
     });
     infoWindows.push(infoWindow);
 
     // when clicking on the marker, all other markers' info windows close
-    marker.addListener('click', function() {
+    marker.addListener('click', function () {
         //infoWindow.open(map, marker);
-        infoWindows.forEach(function(iw) {
+        infoWindows.forEach(function (iw) {
             if (iw !== infoWindow) {
                 iw.close();
-            } else {
+            }
+            else {
                 iw.open(map, marker);
             }
         })
@@ -139,7 +152,8 @@ function attachInfoWindow(patient, marker) {
     marker.addListener('click', function toggleBounce() {
         if (marker.getAnimation() !== null) {
             marker.setAnimation(null);
-        } else {
+        }
+        else {
             marker.setAnimation(google.maps.Animation.BOUNCE);
         }
     });
@@ -162,7 +176,8 @@ function getOrganOptions(patient) {
     var donationStr;
     if (donations !== '[]') {
         donationStr = '<b>Donations:</b><br>' + donations.substring(1, donations.length - 1);
-    } else {
+    }
+    else {
         donationStr = 'No Donations';
     }
 
@@ -171,7 +186,8 @@ function getOrganOptions(patient) {
     var requiredStr;
     if (matching) {
         requiredStr = '<b>Required:</b><br>' + matching.join(', ');
-    } else {
+    }
+    else {
         requiredStr = 'No Requirements';
     }
     return {donating: donationStr, receiving: requiredStr};
@@ -180,7 +196,7 @@ function getOrganOptions(patient) {
 function setPatients() {
     validCount = 0;
 
-    markers.forEach(function(marker) {
+    markers.forEach(function (marker) {
         marker.setMap(null);
     });
     markers = [];
