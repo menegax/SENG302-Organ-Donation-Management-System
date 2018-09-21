892d054f3a070cf81eca6573bd64f53f9332b1a9
Patrick Laffey <plaffey64@gmail.com>
2018-09-21 17:27:44 +1200

    Fixed MathUtilityMethodsTest
    #fix #test #story[1084]

--- src/test/java/utility_test/MathUtilityMethodsTest.java
+++ src/test/java/utility_test/MathUtilityMethodsTest.java
@@ -10,13 +10,13 @@ public class MathUtilityMethodsTest {
 
 	@Test
 	public void testCalculateAngle() {
-		Point2D point1 = new Point2D(50,50);
-		Point2D point2 = new Point2D(123,1);
-		Point2D point3 = new Point2D(594,272);
+		Point2D point1 = new Point2D(5,5);
+		Point2D point2 = new Point2D(1,10);
+		Point2D point3 = new Point2D(7,26);
 		//The correct angle result calculated
-		double angle = 2.028;
+		double angle = 1.034;
 		//The angle calculated from the calculateAngle method
-		double calcAngle = Math.round(MathUtilityMethods.calculateAngle(point1, point2, point3) * 1000) / 1000;
+		double calcAngle = ((double)Math.round((MathUtilityMethods.calculateAngle(point1, point2, point3) * 1000))) / 1000;
 		assertTrue(angle == calcAngle);
 	}
 
@@ -25,7 +25,7 @@ public class MathUtilityMethodsTest {
 		Point2D point1 = new Point2D(115, 25);
 		Point2D point2 = new Point2D(-12, 55);
 		//The correct displacement result calculated
-		double disp = 130.495;
+		double disp = 130.0;
 		//The displacement calculated from the calculateDisplacement method
 		double calcDisp = Math.round(MathUtilityMethods.calculateDisplacement(point1, point2) * 1000) / 1000;
 		assertTrue(disp == calcDisp);
