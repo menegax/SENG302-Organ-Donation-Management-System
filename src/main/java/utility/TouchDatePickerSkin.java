02c3c411dfbf6a3c262d9acbe83825da8588120d
Maree Palmer <mpa588@uclive.ac.nz>
2018-09-24 17:19:35 +1200

    Added rotation and translation for date picker calendar #story[1084] #pair[asm142,pla55,mpa588]

--- src/main/java/utility/TouchDatePickerSkin.java
+++ src/main/java/utility/TouchDatePickerSkin.java
@@ -1,41 +1,41 @@
 package utility;
 
 import com.sun.javafx.scene.control.skin.DatePickerSkin;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
+import javafx.scene.control.ComboBoxBase;
 import javafx.scene.control.DatePicker;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Transform;
+import org.tuiofx.widgets.skin.MTComboBoxListViewSkin;
+import org.tuiofx.widgets.utils.Util;
 
 public class TouchDatePickerSkin extends DatePickerSkin {
 
+    /**
+     * Creates a new skin for a date picker for multi touch purposes.
+     * @param datePicker datepicker to skin
+     * @param pane pane the datepicker contains
+     */
     public TouchDatePickerSkin(DatePicker datePicker, Pane pane) {
         super(datePicker);
         StackPane arrowButtonAlias = (StackPane) datePicker.lookup(".arrow-button");
         arrowButtonAlias.setStyle("-fx-padding: 0 15 0 15;");
         getPopupContent().focusedProperty().addListener((observable, oldValue, newValue) -> {
-            System.out.println(datePicker.getLayoutX());
-            System.out.println(datePicker.getLayoutY());
-            System.out.println(datePicker.getTranslateX());
-            System.out.println(datePicker.getTranslateY());
-            System.out.println(datePicker.getScaleX());
-            System.out.println(datePicker.getScaleY());
-
-            System.out.println(pane.getLayoutX());
-            System.out.println(pane.getLayoutY());
-            System.out.println(pane.getTranslateX());
-            System.out.println(pane.getTranslateY());
-            System.out.println(pane.getScaleX());
-            System.out.println(pane.getScaleY());
-
-            System.out.println(getPopupContent().getLayoutX());
-            System.out.println(getPopupContent().getLayoutY());
-            System.out.println(getPopupContent().getTranslateX());
-            System.out.println(getPopupContent().getTranslateY());
-            System.out.println(getPopupContent().getScaleX());
-            System.out.println(getPopupContent().getScaleY());
-
-            getPopupContent().setRotate(pane.getRotate());
-            getPopupContent().setTranslateX((223 * pane.getScaleX()) - datePicker.getLayoutX());
-            getPopupContent().setTranslateX((389 * pane.getScaleY()) - datePicker.getLayoutY());
+            Node owner = getSkinnable();
+            double offsetY = (getSkinnable()).prefHeight(-1.0D);
+            double angle = Util.getRotationDegreesLocalToScene(owner);
+            getPopupContent().getTransforms().setAll(new Rotate(angle));
+            Rotate rotate = new Rotate(angle);
+            Point2D transformedPoint = rotate.transform(0.0D, offsetY);
+            double popupTopLeftX = owner.getLocalToSceneTransform().getTx();
+            double popupTopLeftY = owner.getLocalToSceneTransform().getTy();
+            double anchorX = popupTopLeftX + transformedPoint.getX() + Util.getOffsetX(owner);
+            double anchorY = popupTopLeftY + transformedPoint.getY() + Util.getOffsetY(owner);
+            getPopup().setAnchorX(anchorX);
+            getPopup().setAnchorY(anchorY);
         });
         getPopup().setAutoHide(false);
         pane.setOnTouchPressed(event -> {
