8a6be0d7623253b71a5d0dfe1308b35518ab3c06
Andrew Spearman <asp79@uclive.ac.nz>
2018-09-25 13:55:56 +1200

    Merged dev in, fixed a few issues
    #story[1112]

--- src/main/java/utility/MapBridge.java
+++ src/main/java/utility/MapBridge.java
@@ -1,6 +1,5 @@
 package utility;
 
-import controller.GUIAvailableOrgans;
 import com.google.maps.errors.ApiException;
 import controller.GUIHome;
 import controller.GUIMap;
@@ -8,19 +7,15 @@ import controller.ScreenControl;
 import javafx.scene.control.ProgressBar;
 import model.Patient;
 import model.PatientOrgan;
-import netscape.javascript.JSObject;
-import model.PatientOrgan;
 import service.PatientDataService;
 import service.interfaces.IPatientDataService;
 
-import java.lang.reflect.Array;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
-import java.io.IOException;
-import java.util.Set;
 
 /**
  * Provides the map javascript access to the java codebase
@@ -30,11 +25,11 @@ public class MapBridge {
     private IPatientDataService patientDataService = new PatientDataService();
 
     private ScreenControl screenControl = ScreenControl.getScreenControl();
-    private PatientDataService patientDataService = new PatientDataService();
 
 
     /**
      * Opens the patient profile in a new window
+     *
      * @param patientNhi The NHI of the patient for  the new GUIHome scene to use
      */
     @SuppressWarnings("unused") // used in corresponding javascript
@@ -44,6 +39,7 @@ public class MapBridge {
         controller.setTarget(patient);
     }
 
+
     /**
      * Collects the patient list from available organs list
      */
@@ -52,8 +48,10 @@ public class MapBridge {
         List<Patient> deadPatients = patientDataService.getDeadDonors();
         for (Patient patient : deadPatients) {
             if (patient.getDeathDate() != null) {
-                for (GlobalEnums.Organ organ : patient.getDonations().keySet()) {
-                    if (patient.getDonations().get(organ) == null) {
+                for (GlobalEnums.Organ organ : patient.getDonations()
+                        .keySet()) {
+                    if (patient.getDonations()
+                            .get(organ) == null) {
                         PatientOrgan patientOrgan = new PatientOrgan(patient, organ);
                         if (!masterData.contains(patientOrgan)) {
                             if (patientOrgan.timeRemaining() < 0) {
@@ -66,57 +64,76 @@ public class MapBridge {
         }
 
         Set<Patient> uniqueSetOfPatients = new HashSet<Patient>();
+
+        for (PatientOrgan aMasterData : masterData) {
+            uniqueSetOfPatients.add(aMasterData.getPatient());
+        }
+        return new ArrayList<>(uniqueSetOfPatients);
+    }
+
+
     /**
      * Checks if the donor patient has organs that have been matched to a recipient patient, if so, triggers js method to
      * create a line between two markers
+     *
      * @param geolocation - lat lng of donor patient
-     * @param patientNhi - nhi number of donor patient
-     * @throws InterruptedException
-     * @throws ApiException
-     * @throws IOException
+     * @param patientNhi  - nhi number of donor patient
+     * @exception InterruptedException
+     * @exception ApiException
+     * @exception IOException
      */
     @SuppressWarnings("unused") // used in corresponding javascript
     public void checkOrganMatch(double[] geolocation, String patientNhi) throws InterruptedException, ApiException, IOException {
         Patient patient = patientDataService.getPatientByNhi(patientNhi);
-        Set<GlobalEnums.Organ> donations = patient.getDonations().keySet();
+        Set<GlobalEnums.Organ> donations = patient.getDonations()
+                .keySet();
 
         if (donations.size() > 0) {
             for (GlobalEnums.Organ organ : donations) {
-                String recipientNhi = patient.getDonations().get(organ);
+                String recipientNhi = patient.getDonations()
+                        .get(organ);
                 if (recipientNhi != null) {
                     Patient recipient = patientDataService.getPatientByNhi(recipientNhi);
                     PatientOrgan targetPatientOrgan = new PatientOrgan(patient, organ);
                     targetPatientOrgan.startTask();
-                    targetPatientOrgan.getProgressTask().setProgressBar(new ProgressBar()); //dummy progress task
-                    CachedThreadPool.getCachedThreadPool().getThreadService().submit(targetPatientOrgan.getProgressTask());
+                    targetPatientOrgan.getProgressTask()
+                            .setProgressBar(new ProgressBar()); //dummy progress task
+                    CachedThreadPool.getCachedThreadPool()
+                            .getThreadService()
+                            .submit(targetPatientOrgan.getProgressTask());
 
-                    GUIMap.getJSBridge().call("matchedOrgan", patient.getCurrentLocation(),
-                            recipient.getCurrentLocation(), recipient.getNhiNumber(),
-                            targetPatientOrgan.getProgressTask().getColor(), organ.toString());
+                    GUIMap.getJSBridge()
+                            .call("matchedOrgan",
+                                    patient.getCurrentLocation(),
+                                    recipient.getCurrentLocation(),
+                                    recipient.getNhiNumber(),
+                                    targetPatientOrgan.getProgressTask()
+                                            .getColor(),
+                                    organ.toString());
 
-                    targetPatientOrgan.getProgressTask().messageProperty().addListener((observable, oldValue, newValue) -> {
-                        if (!oldValue.equals("")) { // first circle always gives green
-                            String color = targetPatientOrgan.getProgressTask().getColor();
-                            GUIMap.getJSBridge().call("updateMatchedOrganLine", color, recipientNhi, organ.toString());
-                        }
-                    });
+                    targetPatientOrgan.getProgressTask()
+                            .messageProperty()
+                            .addListener((observable, oldValue, newValue) -> {
+                                if (!oldValue.equals("")) { // first circle always gives green
+                                    String color = targetPatientOrgan.getProgressTask()
+                                            .getColor();
+                                    GUIMap.getJSBridge()
+                                            .call("updateMatchedOrganLine", color, recipientNhi, organ.toString());
+                                }
+                            });
                 }
             }
         }
 
     }
 
-        for (int i = 0; i < masterData.size(); i++) {
-            uniqueSetOfPatients.add(masterData.get(i).getPatient());
-        }
-        List<Patient> patients = new ArrayList<Patient>(uniqueSetOfPatients);
-        return patients;
-    }
 
-    public void updateInfoWindow(Patient patient){
+    public void updateInfoWindow(Patient patient) {
         if (GUIMap.getJSBridge() != null) {
-            GUIMap.getJSBridge().call("reloadInfoWindow", patient);
-        } else {
+            GUIMap.getJSBridge()
+                    .call("reloadInfoWindow", patient);
+        }
+        else {
             SystemLogger.systemLogger.log(Level.WARNING, "GUIMAP not instantiated - soz for hacky", this);
         }
     }
